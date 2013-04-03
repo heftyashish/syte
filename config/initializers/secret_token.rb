28f9a4e2e6d1fb2ccd55b8b13c9c22adcebe077a
KC <gandhi@linuxmint.linuxmint-domain>
2013-04-03 14:15:44 +0530

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Syte::Application.config.secret_token = '96a33eae34787cd6ee1a166e8f6bcbcfaf3cdf581c0f651f4eeaeaad74c4ddfc5d7dc0b7958e83da33ef3b3220a129b3e015cc26cf7207ee86f67d9546c4a254'
