80f528a253a2e749ea411fd7e3bb4c18d4f729bd
root <root@linuxmint.linuxmint-domain>
2013-04-22 00:20:00 +0530

    more changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :price, :name, :image, :description, :old_price
+  attr_accessible :price, :name, :image, :description
   mount_uploader :image, ImageUploader
   has_many :comments, :dependent=> :destroy
 end
